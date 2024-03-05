6c3edde13eeeb87361b1c405f597fe03896dd945
Mirabell-S <162373836+Mirabell-S@users.noreply.github.com>
2024-03-05 22:01:04 +0100

    Update Bookings.sql

--- Bookings.sql
+++ Bookings.sql
@@ -9,3 +9,14 @@ INNER JOIN `platformbooking.booking.payments`p ON u.user_id = p.user_id
   WHERE b.user_id IS NULL
   GROUP BY u.user_id, u.country_code;
 
+-- The finance team would like to  know the total payments by each user who made payments but hasn't made any bookings.
+SELECT 
+   u.user_id
+  ,COUNT(p.id) AS payment_count, 
+   SUM(p.payment_amount) AS total_payment_amount
+FROM `platformbooking.booking.users` u
+LEFT JOIN `platformbooking.booking.payments`p ON u.user_id = p.user_id
+GROUP BY u.user_id, u.country_code
+HAVING payment_count >=1;
+
+-- It would be nice to include where these users are located.
